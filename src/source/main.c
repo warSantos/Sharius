5c131be957a85ae517c1c539021cb2509d4427f4
Welton <weltonarsantos@gmail.com>
2017-07-25 22:39:00 -0300

    Função de ajuda para menssagem funcionando.

--- src/source/main.c
+++ src/source/main.c
@@ -19,6 +19,10 @@
  *      help (Mostra comandos disponíveis).
  */
 
+// Ajuda para esclarecimento de envio de mensagens, contém
+// exemplos de todas as opções de envio e comandos extras.
+void ajudaMenssagem();
+
 int main(void){
     
     Descritor *listaLogin = iniciarLista();
@@ -64,19 +68,47 @@ int main(void){
         // Se estiver no mode de comandos...
         if(alteraModo[0] == '!' && alteraModo[1] == 'c'){ // chama o menu de comados.
             
-            printf("\n\nUtilizei um comando.\n\n");
+            // Se o último comando utilizado nao foi o de alterar para o modo de comando...
+            if(buffer[0] != '!' && buffer[1] != 'c') {
+                
+                printf("\n\nUtilizei um comando.\n\n");
+            }
+            
         }else{ // Modo de envio de menssagem...
             
-            if(listaVazia(listaLogin)){
+            // Se o ultimo comando utilizado não foi o de alterar para o modo de mensagem...
+            if(buffer[0] != '!' && buffer[1] != 'c') {
                 
-                printf("\n\nSua sala esta vazia.\nAdicione pessoas a sala.\n\n");
-            }else{
+                /*
+                if(strncmp(bu)){
                 
-                printf("\n\nEnviei uma mensagem.\n\n");
-            }            
+                }else{
+                    
+                    if(listaVazia(listaLogin)) {
+
+                        printf("\n\nSua sala esta vazia.\nAdicione pessoas a sala.\n\n");
+                    } else {
+
+                        printf("\n\nEnviei uma mensagem.\n\n");
+                    }
+                }
+                */
+                ajudaMenssagem();
+            }
         }
     }
     free(buffer);
     free(alteraModo);
     return 0;
+}
+
+void ajudaMenssagem(){
+    
+    printf("\n\tAjuda.\n\n");
+    printf("\n\tPor padrão ao se conectar a uma sala suas mensagens são enviadas");
+    printf("\n\tpara todos usuários onlines na sala.");
+    printf("\n\n\tPara enviar mensagens em particular adicione @ antes do nick\n");
+    printf("\tdo receptor.");
+    printf("\n\n\t> @FULANO MENSSAGEM...\n\n");
+    printf("\t> !c altera para o modo de comando.\n\n");    
 }
