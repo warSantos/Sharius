9c09557fff60afcfdc37edb9cf0073d6d420c8e5
Welton <weltonarsantos@gmail.com>
2017-07-25 22:10:55 -0300

    Menu básicos para receber comandos!

--- src/source/main.c
+++ src/source/main.c
@@ -1,60 +1,82 @@
 /*
  *
- * Arquivo main uilizado para debug dos modulos. 
+ * Menu de comandos do Servidor.
+ * Modulo de gerenciamento de conexões.
  * 
  */
 
 #include <stdio_ext.h>
-
 #include "../headers/usuario.h"
+
+/*
+ * Lista de comandos.
+ *      
+ *      add (adiciona usuário).
+ *      remove (remove usuário).
+ *      list (lista usuários).
+ *      send (envia mensagem para broadcast) OBS* modo padrão.
+ *      quit (sai do server)
+ *      help (Mostra comandos disponíveis).
+ */
 
 int main(void){
     
-    printf("Iniciando Server...\n");    
     Descritor *listaLogin = iniciarLista();
-    
-    int x = 0;            
-    while(x < 5){
+    char *buffer = calloc(sizeof(char),251);
+    char *alteraModo = calloc(sizeof(char),3);
+    alteraModo[0] = '!';
+    alteraModo[1] = 'c';
+    printf("\n\n\tCarregando configurações...\n\n");
+    // Menu de comandos.
+    while(1){
         
-        printf("Inserção de usuário.\n\n");
-        int rep = inserirUsuario(listaLogin);  
-        if(rep){
+        if(alteraModo[0] == '!' && alteraModo[1] == 'c'){
             
-            printf("Usuário NÃO adicionado...\n");
-        }
-        ++x;
-    }
-    x = 0;
-    imprimirLista(listaLogin);
-    while(x < 0){
-        
-        printf("Pesquise um nick: ");    
-        char *nick = malloc(sizeof(char) * 16);    
-        scanf("%15[^\n]s", nick);
-        __fpurge(stdin);
-        Link aux = pesquisarNick(listaLogin, nick);
-        if(aux == NULL){
-            
-            printf("Usuário inexistente!\n");
+            printf("$ ");
         }else{
             
-            printf("Usuário: %s endereço: %s.\n", aux->nick, aux->ip);
+            printf("> ");
         }
-        ++x;
-        free(nick);
-    }
-    printf("Server iniciado com sucesso...\n");
-    x = 0;
-    while(x < 8){
         
-        printf("Remova um usuário: ");    
-        char *nick = malloc(sizeof(char) * 16);    
-        scanf("%15[^\n]s", nick);
+        scanf("%250[^\n]s", buffer);
         __fpurge(stdin);
-        removeUsuario(listaLogin, nick);
-        imprimirLista(listaLogin);
-        free(nick);
-        ++x;
+                        
+        // Identifica o princípio de um possível comando de alteração de modo.
+        if(buffer[0] == '!'){ 
+            
+            // Altera pro modo de comando.
+            if(buffer[1] == 'c'){
+                
+                system("clear");
+                printf("\n\nModo de comando.\n\n");
+                alteraModo[0] = '!';
+                alteraModo[1] = 'c';
+            // Altera para o modo de mensagem.    
+            }else if(buffer[1] == 'm'){
+                
+                system("clear");
+                printf("\n\nModo de mensagem.\n\n");
+                alteraModo[0] = '!';
+                alteraModo[1] = 'm';
+            }
+        }
+        
+        // Se estiver no mode de comandos...
+        if(alteraModo[0] == '!' && alteraModo[1] == 'c'){ // chama o menu de comados.
+            
+            printf("\n\nUtilizei um comando.\n\n");
+        }else{ // Modo de envio de menssagem...
+            
+            if(listaVazia(listaLogin)){
+                
+                printf("\n\nSua sala esta vazia.\nAdicione pessoas a sala.\n\n");
+            }else{
+                
+                printf("\n\nEnviei uma mensagem.\n\n");
+            }            
+        }
     }
+    free(buffer);
+    free(alteraModo);
     return 0;
 }
